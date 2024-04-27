0d51cb79e2c53b1c97051fbd0a6a4420c3bf9c3f
SanthoshSpark <santhoshdurairaj001@gmail.com>
2024-04-27 15:47:19 +0530

    first commit

---
+++ src/main/java/com/hibernate/demo/Student.java
@@ -0,0 +1,8 @@
+package com.hibernate.demo;
+
+public class Student {
+
+	private long id;
+	private String name;
+	private StudentDetails studentDetails;
+}
---
+++ src/main/java/com/hibernate/demo/StudentDetails.java
@@ -0,0 +1,8 @@
+package com.hibernate.demo;
+
+public class StudentDetails {
+
+	private long id;
+	private String address;
+	private Student student;
+}
